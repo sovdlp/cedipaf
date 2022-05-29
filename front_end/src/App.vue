5ba15727f8bd4362e60dbb85c6b20780fe1f3d50
sovdlp <88692839+sovdlp@users.noreply.github.com>
2022-05-29 15:31:14 -0500

    nuevos cambiso

--- front_end/src/App.vue
+++ front_end/src/App.vue
@@ -1,23 +1,32 @@
 <template>
   <v-app>
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+    <div id="app">
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link> | 
+        <router-link to="/documentos">Remisiones</router-link>
+      </div>
+      <router-view/>
+    </div>
   </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
-</script>
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
